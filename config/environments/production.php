08a68a2b6344bc09413ec59d726faffc935067c1
Devin <devin@reams.me>
2017-09-20 11:15:51 -0600

    enable multisite

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,3 +5,10 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+/** Multisite Network */
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'aqueous-plains-96856.herokuapp.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
